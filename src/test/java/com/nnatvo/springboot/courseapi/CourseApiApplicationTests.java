85295a039c3952542ca7c7bb60ffc909466e6373
nna144 <llina.tvo@gmail.com>
2020-07-14 13:05:08 +0200

    Initial commit

---
+++ src/test/java/com/nnatvo/springboot/courseapi/CourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnatvo.springboot.courseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
